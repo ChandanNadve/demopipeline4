65aa6caec17a790b104c5807fbf66526d20f9817
CHANDANNADVE <chandannadve@gmail.com>
2023-05-26 09:54:09 +0400

    all file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(name="demopipeline4",
+version="0.0.1",
+author="sunny",
+author_email="chandannnadve@gmail.com",
+packages=find_packages(),
+install_requires=['pandas','numpy','flask'
+]
+)
